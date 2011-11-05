e3eb4e7313902224ffe02119199ee20d4c8687a5
Kozo Nishida <kozo.nishida@gmail.com>
2011-11-05 15:44:35 +0900

    just removed comments

--- bundle.el
+++ bundle.el
@@ -27,48 +27,14 @@
   (setq bundle-install-path (format (concat bundle-install-directory "/" (file-name-sans-extension (file-name-nondirectory bundle-install-last-url)))))
 
   (deferred:$
-;    (deferred:process-shell ("git" "clone" bundle-git-clone))
     (deferred:process "git" "clone" bundle-install-last-url bundle-install-path)
-;    (deferred:process ("ls"))
-;  (setq bundle-git-clone (format (concat "git clone " bundle-install-last-url " " bundle-install-path)))
     (deferred:nextc it
       (lambda ()
-;	(message (format "cloning...: %s" bundle-install-last-url))
 	(message (format "cloning...: %s" (concat "git clone " bundle-install-last-url " " bundle-install-path)))
-;    (deferred:nextc it
 	(add-to-list 'load-path bundle-install-path)
-;    (deferred:nextc it
 	(append-to-file (concat "(add-to-list 'load-path \"" bundle-install-path "\")\n") nil bundle-init-path)
-;    (deferred:nextc it
 	(load (concat bundle-install-directory "/" "init-bundle.el"))))
     
     (deferred:error it ;
       (lambda (err)
-	(insert "Can not get a clone! : " err))))
-
-;  (setq bundle-init-path (format (concat bundle-install-directory "/" "init-bundle.el")))
-;  (shell-command bundle-git-clone)
-)
-
-;; (defun bundle-install-svn (&optional url)
-;;   "Install elisp files from a given url using svn."
-;;   (interactive)
-;;   (or url (setq url (read-string (format "URL (%s):" (or bundle-install-last-url "")) nil nil bundle-install-last-url)))
-;;   (setq bundle-install-last-url url)
-;;   (message (format "cloning...: %s" bundle-install-last-url))
-;;   (setq bundle-install-path (format (concat bundle-install-directory "/" (file-name-sans-extension (file-name-nondirectory bundle-install-last-url)))))
-;;   (setq bundle-svn-checkout (format (concat "svn checkout " bundle-install-last-url " " bundle-install-path)))
-;;   (setq bundle-init-path (format (concat bundle-install-directory "/" "init-bundle.el")))
-;;   (shell-command bundle-svn-checkout)
-;;   (add-to-list 'load-path bundle-install-path)
-;;   (append-to-file (concat "(add-to-list 'load-path \"" bundle-install-path "\")") nil bundle-init-path)
-;; )
-
-;; (defun bundle-install-emacs-w3m ()
-;;   "Install emacs-w3m from cvs repository."
-;;   (interactive)
-;;   (setq bundle-init-path (format (concat bundle-install-directory "/" "init-bundle.el")))
-;;   (shell-command "cvs -d :pserver:anonymous@cvs.namazu.org:/storage/cvsroot co emacs-w3m")
-;;   (add-to-list 'load-path (concat bundle-install-directory "/" "emacs-w3m"))
-;;   (append-to-file (concat "(add-to-list 'load-path \"" (concat bundle-install-directory "/" "emacs-w3m" "\")") nil bundle-init-path))
-;; )
+	(insert "Can not get a clone! : " err)))))
